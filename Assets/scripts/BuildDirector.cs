65085bfb9156417694e3a77313fec9f208bf6611
Mike Brascome <mikebrascome@gmail.com>
2016-07-21 18:05:02 -0500

    Added build code

--- Assets/scripts/BuildDirector.cs
+++ Assets/scripts/BuildDirector.cs
@@ -1,13 +1,41 @@
 ﻿using UnityEditor;
 using System.Collections;
+using System;
+using System.Collections.Generic;
 
 public class BuildDirector {
 
-    static void PerformBuild()
+    static string[] SCENES = FindEnabledEditorScenes();
+
+    static string APP_NAME = "MagicCube";
+    static string TARGET_DIR = "Build";
+
+    [MenuItem("Custom/CI/Build Mac OS X")]
+    static void PerformWinBuild()
     {
-        string localPath = "";
-        string[] scenes = { "Assets/main.unity" };
-        BuildPipeline.BuildPlayer(scenes,localPath,BuildTarget.StandaloneWindows,BuildOptions.None);
+        string target_dir = APP_NAME + ".app";
+        GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneWindows, BuildOptions.None);
+    }
+
+    private static string[] FindEnabledEditorScenes()
+    {
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            EditorScenes.Add(scene.path);
+        }
+        return EditorScenes.ToArray();
+    }
+
+    static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+    {
+        EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+        string res = BuildPipeline.BuildPlayer(scenes, target_dir, build_target, build_options);
+        if (res.Length > 0)
+        {
+            throw new Exception("BuildPlayer failure: " + res);
+        }
     }
 
 
