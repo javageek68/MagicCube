1ecc19d6de310c50c1ec97417e079c576dad6bdd
Mike Brascome <mikebrascome@gmail.com>
2016-07-21 17:56:37 -0500

    added build director

---
+++ Assets/scripts/BuildDirector.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using System.Collections;
+
+public class BuildDirector {
+
+    static void PerformBuild()
+    {
+        string localPath = "";
+        string[] scenes = { "Assets/main.unity" };
+        BuildPipeline.BuildPlayer(scenes,localPath,BuildTarget.StandaloneWindows,BuildOptions.None);
+    }
+
+
+}
